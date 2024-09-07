5a7d4549658eeee6748073a0f6106ea9cae47299
Sanket Shetty <legendhimself007@gmail.com>
2024-09-08 01:07:37 +0530

    Animations

---
+++ script.js
@@ -0,0 +1,9 @@
+// Text Animation
+
+// const button = document.querySelector('button');
+
+// button.addEventListener('click', ()=>{
+//     button.classList.toggle('change-color-btn')
+// });
+
+// Dark Mode
